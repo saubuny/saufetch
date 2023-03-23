bee45264a001f384145c261ee9e871be307d4ad3
saubuny <122068609+saubuny@users.noreply.github.com>
2023-03-23 10:13:35 -0400

    Fix dotted line length

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use sysinfo::{System, SystemExt, UserExt};
 
 pub mod ascii_art;
 
+// TODO: Have less info shown with small option
 #[derive(Parser, Debug)]
 struct Cli {
     #[arg(value_enum)]
@@ -14,26 +15,30 @@ struct Cli {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
-    // Eventually change this to just the things we need, and not everything
+    // TODO: Change this to just the things we need, and not everything
     let mut sys = System::new_all();
 
     sys.refresh_all();
 
-    // All art should be 16 lines long
+    // NOTE: All large art should be 16 lines long
     let ascii_art = get_ascii_art(args.ascii_art_size);
     let width = longest_str(&ascii_art) + 5;
 
     println!();
 
-    let name = format!(
-        "{}@{}",
-        sys.users().get(2).unwrap().name().green().bold(),
-        sys.host_name().unwrap().green().bold()
-    );
+    // Colored strings add invisible length to the string, but we need
+    // the actual length later
+    let name1 = sys.users().get(2).unwrap().name();
+    let name2 = sys.host_name().unwrap();
+    let name = format!("{}@{}", name1.green().bold(), name2.green().bold());
 
     println!("{:<width$}{}", ascii_art[0], name);
 
-    println!("{:<width$}{}", ascii_art[1], "-".repeat(name.len()));
+    println!(
+        "{:<width$}{}",
+        ascii_art[1],
+        "-".repeat(name1.len() + name2.len() + 1)
+    );
 
     println!(
         "{:<width$}{:<10}{:>10}",
